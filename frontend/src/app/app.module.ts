c0e669017231d86718870a0329beeb4eac60dc4d
sergey-laschotko <anaharet@tut.by>
2019-04-03 01:22:08 +0300

    added ethernet-settings and wifi-settings components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EthernetSettingsComponent } from './ethernet-settings/ethernet-settings.component';
+import { WifiSettingsComponent } from './wifi-settings/wifi-settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EthernetSettingsComponent,
+    WifiSettingsComponent
   ],
   imports: [
     BrowserModule
