d9e2800106dc525aceeb9d97f4fa3d9a710dc0f8
sergey-laschotko <anaharet@tut.by>
2019-04-03 09:45:10 +0300

    marked general settings and wifi settings

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EthernetSettingsComponent } from './ethernet-settings/ethernet-settings.component';
 import { WifiSettingsComponent } from './wifi-settings/wifi-settings.component';
+import { GeneralSettingsComponent } from './general-settings/general-settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EthernetSettingsComponent,
-    WifiSettingsComponent
+    WifiSettingsComponent,
+    GeneralSettingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
